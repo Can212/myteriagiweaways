c8c6908fcaf34a1f3d850b3b2f437a117e5cc3ad
sedat0611 <74787493+sedat0611@users.noreply.github.com>
2021-05-19 16:18:52 +0300

    Update çekiliş-düzenle.js

--- komutlar/çekiliş-düzenle.js
+++ komutlar/çekiliş-düzenle.js
@@ -10,11 +10,11 @@ exports.run = async (award, message, args) => {
             return;
           }
     
-    let gweep = args[0];
+    let myteria = args[0];
     let kazanan = args[1];
     let odul = args[2];
     let zaman = args[3];
-    if(!gweep) return message.channel.send("Please specify a valid message ID! (Usage: <MessageID> <WinnersAmount> <Prize> <TimeValue>)")
+    if(!myteria) return message.channel.send("Please specify a valid message ID! (Usage: <MessageID> <WinnersAmount> <Prize> <TimeValue>)")
     if(!kazanan) return message.channel.send("You didn't provide a valid amount of winners! (Usage: <MessageID> <WinnersAmount> <Prize> <TimeValue>)")
     if(!odul) return message.channel.send("You didn't specify a prize! (Usage: <MessageID> <WinnersAmount> <Prize> <TimeValue>)")
     if(!ms(zaman) && zaman) return message.channel.send("You didn't use a valid time format! (Usage: <MessageID> <WinnersAmount> <Prize> <TimeValue>)")
